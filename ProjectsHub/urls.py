f30eab7c3644d4a7f4aa86f87ee706c8420f5bde
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-16 21:59:47 +0200

    add projects router

--- ProjectsHub/urls.py
+++ ProjectsHub/urls.py
@@ -1,1 +1,12 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from ProjectsHub.views import *
+
+
+
+router = DefaultRouter()
+router.register('projects', ProjectViewSet, basename='projects')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
