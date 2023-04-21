c04af87f1c02aa12db63248d32e077901ca18a43
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-22 00:53:18 +0200

    add search projects endpoint

--- ProjectsHub/views.py
+++ ProjectsHub/views.py
@@ -3,9 +3,11 @@ from ProjectsHub.serializers import *
 from ProjectsHub.models import *
 from ProjectsHub.pagination import *
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 class ProjectViewSet(ModelViewSet):
     pagination_class = DefaultPagination
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['registration__open_course__semester__semester_name']
     filterset_fields = [ 'registration__open_course__course__course_name', 'registration__open_course__semester__semester_name', 'registration__open_course__course__level__level_name','registration__open_course__course__department__department_name']
 
     
