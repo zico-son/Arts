a927b0cec501a3572e661f064cb60ff64175dd7c
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-25 00:54:37 +0200

    update User model attributes

--- Core/models.py
+++ Core/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
-
-def o6u_email_validator(value):
-    if '@o6u.edu.eg' in value:
-        return value
-    else:
-        raise ValidationError('Please use your O6U email address')
-
+from Core.validators import o6u_email_validator
 
 class User(AbstractUser):
-    # email = models.EmailField(validators=[o6u_email_validator], unique=True)
-    # account_type = models.CharField(max_length=50, default='student')
-    pass
+    email = models.EmailField(validators=[o6u_email_validator], unique=True)
+    account_type = models.CharField(max_length=50, default='student')
