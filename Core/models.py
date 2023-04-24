8b33d31b009f3a63d277e8e1b517584f9e7cdd46
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-25 01:24:22 +0200

    add AccountType model

--- Core/models.py
+++ Core/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from Core.validators import o6u_email_validator
 
+class AccountType(models.Model):
+    account_type = models.CharField(max_length=50, unique=True)
+
 class User(AbstractUser):
     email = models.EmailField(validators=[o6u_email_validator], unique=True)
-    account_type = models.CharField(max_length=50, default='student')
+    account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE, default=1)
