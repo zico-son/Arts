38071da0fd68f9dee9b929db01ca09c37ddbca81
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-07 01:13:10 +0300

    fix student endpoints order

--- ProjectsHub/urls.py
+++ ProjectsHub/urls.py
@@ -7,19 +7,19 @@ from . import views
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet, basename='projects')
-router.register('student-projects', StudentProjectViewSet, basename='student-projects')
 router.register('semesters', SemesterViewSet, basename='semesters')
 router.register('instructors', InstructorViewSet, basename='instructors')
 router.register('departments', DepartmentViewSet, basename='departments')
 router.register('levels', LevelViewSet, basename='levels')
 router.register('courses', CourseViewSet, basename='courses')
-router.register('student-courses', StudentCourseViewSet, basename='student-courses')
 router.register('open-courses', OpenCourseViewSet, basename='open-courses')
 router.register('students', StudentViewSet, basename='students')
 router.register('registrations', CourseRegistrationViewSet, basename='registrations')
 router.register('join-course',JoinCourseViewSet,basename='join-course')
 router.register('instructor-courses',InstructorCourseViewSet,basename='instructor-courses')
 router.register('instructor-projects',InstructorProjectsViewSet,basename='instructor-projects')
+router.register('student-courses', StudentCourseViewSet, basename='student-courses')
+router.register('student-projects', StudentProjectViewSet, basename='student-projects')
 urlpatterns = [
     path('', include(router.urls)),
     
