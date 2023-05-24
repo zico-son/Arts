6b0ac5c51de6d23c26a49ec4f946773292cdad2c
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-24 11:42:38 +0300

    set SendGrid email

--- ArtsGallery/settings.py
+++ ArtsGallery/settings.py
@@ -163,3 +163,10 @@ DJOSER = {
     'current_user': 'Core.serializers.UserSerializer'
     }   
 }
+
+DEFAULT_FROM_EMAIL = 'zicocomlimited@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' 
+EMAIL_HOST_PASSWORD = env('SENDGRID_API_KEY')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
