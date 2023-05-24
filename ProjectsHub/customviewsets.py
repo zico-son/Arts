12af674051d3d2ad489ab5bbb959d197e5ffe86c
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-24 11:32:04 +0300

    add NoDeleteModelViewSet

--- ProjectsHub/customviewsets.py
+++ ProjectsHub/customviewsets.py
@@ -1,6 +1,10 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 class CustomModelViewSet(ListModelMixin,RetrieveModelMixin,DestroyModelMixin,GenericViewSet):
     # This class is used to create a custom viewset that has the list, retrieve and destroy actions
+    pass
+
+class NoDeleteModelViewSet(ListModelMixin,RetrieveModelMixin,UpdateModelMixin,CreateModelMixin,GenericViewSet):
+    # This class is used to create a custom viewset that has the list, retrieve and update actions
     pass
