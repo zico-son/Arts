f387e20a57975459a98267c8ec1c22066392ff42
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-24 11:16:30 +0300

    update Core User model account_type

--- Core/models.py
+++ Core/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from Core.validators import o6u_email_validator
 
-class AccountType(models.Model):
-    account_type = models.CharField(max_length=50, unique=True)
-
 class User(AbstractUser):
+    student = 'student'
+    instructor = 'instructor'
+    admin = 'admin'
+    account_type_choices = [
+        (student, 'student'),
+        (instructor, 'instructor'),
+        (admin, 'admin'),
+    ]
     email = models.EmailField(validators=[o6u_email_validator], unique=True)
-    account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE, default=1)
+    account_type = models.CharField(max_length=10, choices=account_type_choices, default=student)
